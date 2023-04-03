f7dbca384d488e84bc1c9bb84815e0e09e1afb9a
Zhen Nie <znie@smith.edu>
2023-04-03 09:54:02 -0400

    advance function 0403

---
+++ advfunction.R
@@ -0,0 +1,15 @@
+# code along ----
+# to test making our own method
+
+# make an example vector
+test_vec = c(TRUE, FALSE, TRUE, TRUE, FALSE)
+
+# assign this example vector a class
+class(test_vec) = "Apple"
+
+# make a print method "apple" class
+print.Apple = function(x){
+  
+  
+  
+}
